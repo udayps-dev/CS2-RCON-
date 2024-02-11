29df0b4a5d2d5c6c39bb7c19da11fc200ae9cd96
Uday PS <contact@udayps.com>
2024-02-11 20:39:48 +0530

    message

--- db.js
+++ db.js
@@ -20,8 +20,8 @@ better_sqlite_client.exec(`
   )
 `);
 
-const default_username = 'cspanel';
-const default_password = 'v67ic55x4ghvjfj';
+const default_username = 'uday';
+const default_password = 'ps';
 // Hash the default password
 const hashed_password = bcrypt.hashSync(default_password, 10);
 
